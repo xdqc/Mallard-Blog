f1074513768b1783fd419b07bec96f94d206b8a7
Tonny <yyl15@waikato.ac.nz>
2018-02-05 10:10:25 +1300

    the multimedia gallery and atctivate picture

--- web/javascript/uploadFile.js
+++ web/javascript/uploadFile.js
@@ -1,5 +1,5 @@
-//const entityId = (e) => $(e).attr("id").slice($(e).attr("id").lastIndexOf("-")+1, $(e).attr("id").length);
-const entityParameterName = (e) => $(e).attr("id").split("-")[$(e).attr("id").split("-").length-2];
+const getEntityId = (e) => $(e).attr("id").slice($(e).attr("id").lastIndexOf("-")+1, $(e).attr("id").length);
+const getEntityParameterName = (e) => $(e).attr("id").split("-")[$(e).attr("id").split("-").length-2];
 const isUserCheck = (e) => $(e).attr("id").split("-")[$(e).attr("id").split("-").length-3];
 //add a input button to upload area
 function addFileInput(attachmentId,parameterName) {
@@ -35,61 +35,74 @@ function showArticles(parameterName,value) {
 }
 
 $(document).ready(function () {
+
     //deal with the multimedia gallery show content
     //also works on ajax loaded part
     $(document).on("click", ".show-media", function() {
-        const attachmentId = entityId($(this));
-        const parameterName = entityParameterName($(this));
+        const entityId = getEntityId($(this));
+        const parameterName = getEntityParameterName($(this));
         const userCheck = isUserCheck($(this));
+        alert(userCheck);
         if(userCheck.startsWith("UserCheck")){
            const userID = userCheck.split("_")[1];
-            alert(userID);
             $.ajax({
                 url : 'multimedia-gallery',
                 data : {
-                    attachmentId : attachmentId,
+                    entityId : entityId,
                     parameterName : parameterName,
                     userID : userID
                 },
                 success : function(responseText) {
-                    $('#multimediaShowArea-' + parameterName + '-'+attachmentId).html(responseText);
+                    $('#multimediaShowArea-' + parameterName + '-'+entityId).html(responseText);
                 }
             });
         }else if(userCheck.toLowerCase() == "filelist" ){
             $.ajax({
                 url : 'File-Manage',
                 data : {
-                    attachmentId : attachmentId,
+                    entityId : entityId,
                     parameterName : parameterName,
                     operateName : userCheck
                 },
                 success : function(responseText) {
-                    $('#multimediaShowArea-' + parameterName + '-'+attachmentId).html(responseText);
+                    $('#multimediaShowArea-' + parameterName + '-'+entityId).html(responseText);
                 }
             });
         }else if(userCheck.toLowerCase() == "delete" ){
             $.ajax({
                 url : 'File-Manage',
                 data : {
-                    attachmentId : attachmentId,
+                    entityId : entityId,
                     parameterName : parameterName,
                     operateName : userCheck
                 },
                 success : function(responseText) {
-                    $('#multimediaShowArea-' + parameterName + '-'+attachmentId).html(responseText);
+                    $('#multimediaShowArea-' + parameterName + '-'+entityId).html(responseText);
                 }
             });
-        }else if(userCheck ==null || userCheck.toLowerCase() == "" ){
+        }else if(userCheck.toLowerCase() == "showmultimedia" ){
         $.ajax({
             url : 'multimedia-gallery',
             data : {
-                attachmentId : attachmentId,
+                entityId : entityId,
                 parameterName : parameterName
             },
             success : function(responseText) {
                 $('#multimediaShowArea-' + parameterName + '-'+attachmentId).html(responseText);
             }
         });
+        }else if(userCheck.toLowerCase() == "activatelist" ){
+            $.ajax({
+                url: 'Attachment-Manage',
+                data: {
+                    entityId: entityId,
+                    parameterName: parameterName,
+                    operateName : userCheck
+                },
+                success: function (responseText) {
+                    $('#multimediaShowArea-' + parameterName + '-' + entityId).html(responseText);
+                }
+            });
         }
     });
 
